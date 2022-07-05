24ee407a2ee0b739ace3ca65aa099fde161e1a33
cecilia-tisca <ceciliatisca@gmail.com>
2022-07-05 08:58:58 -0500

    feat: File Creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./index.js";
 
-function App() {
+export default function App() {
+const [pokemons, setPokemons] = useState([])
+
+  useEffect(() =>{
+    listPokemons()
+  },[])
+   
+  async function listPokemons(){
+    const result = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+    const data = await result.json();
+    console.log(data)
+    setPokemons(data.result)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemón</h1>
+          <ul>
+            {
+              (pokemons || []).map((pokemon)=><li key={pokemon.name}>{pokemon.name}</li>)
+            }
+          </ul>
     </div>
   );
 }
-
-export default App;
